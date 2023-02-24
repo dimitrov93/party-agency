01a863a16d07c4ed157b35fe1542feda58b53073
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-24 22:55:10 +0200

    cards comp

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,13 +1,97 @@
 import React from "react";
 import "./card.css";
-import img1 from "../../assets/Cards/table/main.jpg";
-import img11 from "../../assets/Cards/table/footer.jpg";
-import img2 from "../../assets/Cards/tablo/main.jpg";
-import img21 from "../../assets/Cards/tablo/footer.jpg";
-import img3 from "../../assets/Cards/cards/main.jpg";
-import img31 from "../../assets/Cards/cards/footer.jpg";
+// Table
+import mainTable from "../../assets/Cards/table/main.jpg";
+import table1 from "../../assets/Cards/table/1.jpg";
+import table2 from "../../assets/Cards/table/2.jpg";
+import table3 from "../../assets/Cards/table/3.jpg";
+import table4 from "../../assets/Cards/table/4.jpg";
+import table5 from "../../assets/Cards/table/5.jpg";
+
+//Tablo
+import mainTablo from "../../assets/Cards/tablo/main.jpg";
+import tablo1 from "../../assets/Cards/tablo/1.jpg";
+import tablo2 from "../../assets/Cards/tablo/2.jpg";
+import tablo3 from "../../assets/Cards/tablo/3.jpg";
+import tablo4 from "../../assets/Cards/tablo/4.jpg";
+import tablo5 from "../../assets/Cards/tablo/5.jpg";
+import tablo6 from "../../assets/Cards/tablo/6.jpg";
+import tablo7 from "../../assets/Cards/tablo/7.jpg";
+import tablo8 from "../../assets/Cards/tablo/8.jpg";
+
+//Cards
+import mainCard from "../../assets/Cards/cards/main.jpg";
+import card1 from "../../assets/Cards/cards/1.jpg";
+import card2 from "../../assets/Cards/cards/2.jpg";
+import card3 from "../../assets/Cards/cards/3.jpg";
+import Invitations from "./Invitations/Invitations";
+import Tablo from "./Tablo/Tablo";
+import Table from "./Table/Table";
+
+
 
 const Card = () => {
+
+    const tabloImgs = [
+        {
+            img: tablo1
+        },
+        {
+            img: tablo2
+        },
+        {
+            img: tablo3
+        },
+        {
+            img: tablo4
+        },
+        {
+            img: tablo5
+        },
+        {
+            img: tablo6
+        },
+        {
+            img: tablo7
+        },
+        {
+            img: tablo8
+        },
+    ]
+    
+    const tableImgs = [
+        {
+            img: table1
+        },
+        {
+            img: table2
+        },
+        {
+            img: table3
+        },
+        {
+            img: table4
+        },
+        {
+            img: table5
+        },
+    
+    ]
+    
+    const cardsImgs = [
+        {
+            img: card1
+        },
+        {
+            img: card2
+        },
+        {
+            img: card3
+        },
+        
+    ]
+    
+
   return (
     <section className="container">
       <div class="card__header">
@@ -19,7 +103,7 @@ const Card = () => {
       <div className="card__body">
         <div className="card__items">
           <div className="card__img">
-            <img src={img1} alt="" />
+            <img src={mainTable} alt="" />
           </div>
 
           <div className="card__text">
@@ -32,7 +116,7 @@ const Card = () => {
 
         <div className="card__items">
           <div className="card__img">
-            <img src={img2} alt="" />
+            <img src={mainTablo} alt="" />
           </div>
 
           <div className="card__text">
@@ -45,7 +129,7 @@ const Card = () => {
 
         <div className="card__items">
           <div className="card__img">
-            <img src={img3} alt="" />
+            <img src={mainCard} alt="" />
           </div>
 
           <div className="card__text">
@@ -73,24 +157,16 @@ const Card = () => {
       <div className="card__footer">
         
         <div className="card__footer__container">
-          <img src={img11} alt="" />
-          <div class="image-text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ab?</p>
-          </div>
+          <Table galleryImg={tableImgs} />
         </div>
 
         <div className="card__footer__container">
-          <img src={img21} alt="" />
-          <div class="image-text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ab?</p>
-          </div>
+          <Tablo galleryImg={tabloImgs} />
         </div>
+        
 
         <div className="card__footer__container">
-          <img src={img31} alt="" />
-          <div class="image-text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ab?</p>
-          </div>
+            <Invitations galleryImg={cardsImgs} />
         </div>
       </div>
     </section>
---
+++ src/components/Cards/Table/Table.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import { BsArrowRightCircle, BsArrowLeftCircle } from "react-icons/bs";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import footerTable from "../../../assets/Cards/table/footer.jpg";
+
+const Table = ({ galleryImg }) => {
+  const timerRef = useRef(null);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(galleryImg.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+
+  const nextSlide = () => {
+    slideNumber + 1 === galleryImg.length
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
+
+  useEffect(() => {
+    if (openModal) {
+      timerRef.current = setTimeout(() => {
+        nextSlide();
+      }, 2000);
+    }
+
+    return () => clearTimeout(timerRef.current);
+  });
+
+  return (
+    <div className="pic">
+      {openModal && (
+        <div className="slider__wrap">
+          {" "}
+          <AiOutlineCloseCircle
+            className="btnClose"
+            onClick={handleCloseModal}
+          />
+          <BsArrowLeftCircle className="btnPrev" onClick={prevSlide} />
+          <BsArrowRightCircle className="btnNext" onClick={nextSlide} />
+          <div className="fullscreen__img">
+            <img src={galleryImg[slideNumber].img} alt="" />
+          </div>
+        </div>
+      )}
+
+        <img
+          src={footerTable}
+          alt=""
+          className="static"
+          onClick={() => {
+            handleOpenModal(0);
+          }}
+        />
+        <div class="image-text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ab?
+          </p>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
