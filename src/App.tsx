63c00889d66395bb3b14a9dc10719ae30dd688eb
Tsvetomir Dimitrov <ts.dimitrov93@gmail.com>
2025-01-05 23:49:05 +0200

    routes created for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
-import Navigation from "./components/Navigation/Navigation";
-import Contacts from "./components/Contacts/Contacts";
-import Decoration from "./components/Decoration/Decoration";
-import Album from "./components/Decoration/Album/Album";
-import Card from "./components/Cards/Card";
-import Login from "./components/Login/Login";
-import Logout from "./components/Logout/Logout";
 import { AuthProvider } from "./context/AuthContext";
-import Add from "./components/Decoration/Add/Add";
-import AddCard from "./components/Cards/Add/Add";
-import Edit from "./components/Decoration/Edit/Edit";
-import NotFound from "./pages/NotFound";
+import Navigation from "./components/Navigation/Navigation";
+import { routes } from "./routes";
 
 function App() {
   return (
@@ -20,24 +10,9 @@ function App() {
       <Navigation />
 
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-
-        <Route path="/contacts" element={<Contacts />}></Route>
-        <Route path="/cards" element={<Card />}></Route>
-        <Route path="/cards/add" element={<AddCard />}></Route>
-
-        <Route path="/decoration" element={<Decoration />}></Route>
-        <Route path="/decoration/:albumType" element={<Album />} />
-        <Route path="/decoration/:albumType/add" element={<Add />} />
-        <Route
-          path="/decoration/:albumType/:location/edit"
-          element={<Edit />}
-        />
-
-        <Route path="/api/login" element={<Login />}></Route>
-        <Route path="/api/logout" element={<Logout />}></Route>
-
-        <Route path="*" element={<NotFound />} />
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
       </Routes>
 
       <Footer />
