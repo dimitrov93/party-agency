f8fa147ad8acede2c34021be1d3c6e9b823b3ce7
Tsvetomir Dimitrov <ts.dimitrov93@gmail.com>
2026-04-03 16:02:05 +0300

    Squashed 'server/' content from commit 4860b96
    git-subtree-dir: server
    git-subtree-split: 4860b96779cea78ea12b106490faee6492af2bb5

---
+++ services/authService.js
@@ -0,0 +1,38 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const {SECRET} = require('../config/env')
+
+exports.create = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+    const user = await User.findOne({email});
+
+    if (!user) {
+        throw {message: 'Cannot find email or password!'}
+    };
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw {message: 'Cannot find email or password!'}
+    }
+
+    return user;
+}
+
+
+exports.createToken = (user) => {
+    const payload = {_id: user._id, email: user.email};
+    const options = {expiresIn: "2d"};
+
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, SECRET, options, (err, decodedToken) => {
+            if (err) {
+                console.log('token error ' + err);
+                return reject(err)
+            }
+            resolve(decodedToken)
+        })
+    });
+}
